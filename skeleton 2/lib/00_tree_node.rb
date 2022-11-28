b069aac48abc692433d31ddb39b6fe02af2a4ad8
Yen Lee <yenny0104@gmail.com>
2022-11-28 14:29:03 -0500

    3

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        if !self.children.include?(node)
+            @children << node.children
+        else
+            
+        end
+    end
 end
