7486532b284576a9534fbac170e07fb405aead7f
Yen Lee <yenny0104@gmail.com>
2022-11-28 16:08:51 -0500

    5

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -8,25 +8,35 @@ class PolyTreeNode
     end
 
     def parent=(passed_node) #self is current node 
-
-      if @parent == nil 
+        if passed_node == nil
+            @parent = nil
+            return 
+        end
+        if @parent == nil 
           if !passed_node.children.include?(self)
               passed_node.children << self
           end
            @parent = passed_node
-      else
+        else
             @parent.children.delete(self)
             @parent = passed_node
           if !passed_node.children.include?(self)
               passed_node.children << self
           end
-      end
-      
+        end 
     end
 
+    def add_child(child)
+        if !self.children.include?(child)
+            child.parent=(self)
+        end
+    end
 
-    def add_child(node)
-      self.parent=(node)
+    def remove_child(child)
+        raise "error" if !self.children.include?(child)
+        child.parent=(nil)
+    end
 
+    def dfs
     end
 end
